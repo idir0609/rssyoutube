f2e2e09b0f18cea4949d3b236d09a258b655ef7b
idir0609 <idir0609@gmail.com>
2016-08-15 13:13:14 +0000

    Update yousearch2.py

--- yousearch2.py
+++ yousearch2.py
@@ -3,7 +3,7 @@ import urllib
 
 search = raw_input('search term')
 
-results = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+search+"&type=video&key=AIzaSyCjTKZ7dY0oaTvbABEvPQ26nHkVaqnGtyE")
+results = urllib.urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&order=date&q=gendarmerie%2Broyale&key=AIzaSyBNAkb86snLJdii_8XX2DZti2FBQ0xn8JE")
 youjs = json.load(results)
 print youjs['items'][0]['id']
 print youjs['items'][0]['snippet']['title']
